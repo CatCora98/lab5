3efe1d916f849b35b07d3cd7dbbfdfee4d45cd76
moli1296 <yichendu97@gmail.com>
2019-02-27 13:06:27 -0500

    lab 5 2nd fix

--- app/src/main/java/com/temple/edu/coloractivity/PaletteActivity.java
+++ app/src/main/java/com/temple/edu/coloractivity/PaletteActivity.java
@@ -16,7 +16,7 @@ public class PaletteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Pallete Activity");
+        setTitle("Palette Activity");
 
         Resources res = getResources();
         final String[] colors = res.getStringArray(R.array.colorList);
